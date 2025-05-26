915906927358323e74f9c38a7c830af072f416c3
DinhTQSE <nguyensoi0966622100@gmail.com>
2025-05-26 10:38:17 +0700

    Chiỉnh sửa cấu hình database

--- src/main/java/com/swp391_8/schoolhealth/SchoolHealthApplication.java
+++ src/main/java/com/swp391_8/schoolhealth/SchoolHealthApplication.java
@@ -2,8 +2,13 @@ package com.swp391_8.schoolhealth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
 public class SchoolHealthApplication {
     public static void main(String[] args) {
         SpringApplication.run(SchoolHealthApplication.class, args);
